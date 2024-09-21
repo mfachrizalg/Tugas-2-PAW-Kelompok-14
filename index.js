d1ced3ceb97426a582c8de02e90b0e60569e94c6
dhimasnp <you@example.com>
2024-09-22 00:02:37 +0700

    Adding Feature to List

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ connectDB();
 
 app.use('/book', require('./src/routers/bookRouter'))
 app.use('/feedback', require('./src/routers/feedbackRouter'))
+app.use('/list', require('./src/routers/listRouter'))
 
 mongoose.connection.once("open", () => {
   console.log("Connected to MongoDB");
