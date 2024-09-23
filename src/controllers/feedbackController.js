56eafccec0db79c4ef58591b8d0745e9b15c2179
mfachrizalg <mohammad.fachrizal123@gmail.com>
2024-09-23 21:15:20 +0700

    remove transaction

--- src/controllers/feedbackController.js
+++ src/controllers/feedbackController.js
@@ -1,5 +1,4 @@
 const Feedback = require('../models/Feedback');
-const mongoose = require('mongoose');
 
 // logic untuk melihat feedback yang ada dari buku
 exports.getFeedbackbyBook = async (req, res) => {
@@ -15,42 +14,28 @@ exports.getFeedbackbyBook = async (req, res) => {
 
 // logic untuk menambahkan feedback (butuh autorisasi)
 exports.addFeedback = async (req, res) => {
-    let session;
     const { bookId } = req.query;
     const { feedback, userId } = req.body;
     try {
-        session = await mongoose.startSession();
-        session.startTransaction();
-
         const newFeedback = new Feedback({ feedback, userId, bookId });
-        await newFeedback.save({session});
-        await session.commitTransaction();
+        const saveFeedback = await newFeedback.save();
+        if (!saveFeedback) res.status(400).json({ message: 'Failed to add feedback' });
 
         res.status(200).send(newFeedback);
     } catch (error) {
-        await session.abortTransaction();
         res.status(500).json({ message: error.message });
-    } finally {
-        if (session) session.endSession();
-    }
+    } 
 }
 
 // logic untuk mengupdate feedback (butuh autorisasi)
 exports.updateFeedback = async (req, res) => {
-    let session;
     const { id } = req.params;
     const { feedback } = req.body;
     try {
-        session = await mongoose.startSession();
-        session.startTransaction();
-        await Feedback.findByIdAndUpdate(id, { feedback }, { session });
-        await session.commitTransaction();
+        await Feedback.findByIdAndUpdate(id, { feedback });
 
         res.status(200).json({ message: 'Feedback updated successfully' });
     } catch (error) {
-        await session.abortTransaction();
         res.status(500).json({ message: error.message });
-    } finally {
-        if (session) session.endSession();
     }
 }
