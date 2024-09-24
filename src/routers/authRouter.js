c4b67a548bd4d66cdc72dc54feca0af983b42a29
dellafebialfian <dellafebialfian@mail.ugm.ac.id>
2024-09-24 15:42:06 +0700

    feat: add deleteAccount

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,11 +1,13 @@
 // /routes/authRoutes.js
 const express = require("express");
 const authController = require("../controllers/authController");
+const { authenticateToken } = require("../middlewares/authorize");
 const router = express.Router();
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.get("/logout", authController.logout);
 router.get("/", authController.checkCookie);
+router.delete("/deleteAccount", authenticateToken, authController.deleteAccount);
 
 module.exports = router;
