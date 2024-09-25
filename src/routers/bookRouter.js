f104327990be4ed3a15ecce9d90bc44303e683f8
dellafebialfian <dellafebialfian@mail.ugm.ac.id>
2024-09-25 11:04:32 +0700

    feat: add getAllBooks

--- src/routers/bookRouter.js
+++ src/routers/bookRouter.js
@@ -1,7 +1,8 @@
-const {getBooks} = require('../controllers/bookController');
+const {getBooks, getAllBooks} = require('../controllers/bookController');
 const express = require('express');
 const router = express.Router();
 
 router.get('/', getBooks);
+router.get('/all', getAllBooks);
 
 module.exports = router;
